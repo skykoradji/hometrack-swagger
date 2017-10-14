00ef6905890ec26158b3a2ec1faa1d480246d5f6
skykoradji <rongguiyou@gmail.com>
2017-10-14 15:13:24 +1100

    add comment and bug fix

--- api/controllers/home.js
+++ api/controllers/home.js
@@ -1,10 +1,15 @@
 'use strict';
 
-
-
 const httpError = require('http-errors');
 const _         = require('lodash');
 
+/**
+ * [track description]
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
 function track(req, res, next) {
 
   let payload  = req.swagger.params.payload.value;
@@ -16,7 +21,11 @@ function track(req, res, next) {
   } catch (e) {
     return next(httpError.BadRequest('Could not decode request: JSON parsing failed'));
   }
-
+  //if it is not a list, we return the error
+  if(!_.isArray(payload)) {
+    return next(httpError.BadRequest('Could not decode request: JSON parsing failed'));
+  }
+  //filtering out the corresponding type and workflow
   let filter = _.filter(payload, function(item) {
     return item.type === type && item.workflow === workflow;
   });
